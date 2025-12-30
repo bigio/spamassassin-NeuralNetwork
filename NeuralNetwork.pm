c97d51093a54577fcc3edc2fba5ab459433d6040
Giovanni <giovanni@paclan.it>
2025-12-30 12:23:44 +0100

    Neural Network plugin

---
+++ NeuralNetwork.pm
@@ -0,0 +1,611 @@
+# <@LICENSE>
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to you under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# </@LICENSE>
+#
+# Author: Giovanni Bechis <gbechis@apache.org>
+
+=head1 NAME
+
+Mail::SpamAssassin::Plugin::NeuralNetwork - check messages using Fast Artificial Neural Network library
+
+=head1 SYNOPSIS
+
+  loadplugin Mail::SpamAssassin::Plugin::NeuralNetwork
+
+=head1 DESCRIPTION
+
+This plugin checks emails using Neural Network algorithm.
+
+=cut
+
+package Mail::SpamAssassin::Plugin::NeuralNetwork;
+
+use strict;
+use warnings;
+use re 'taint';
+
+my $VERSION = 0.1;
+
+use AI::FANN qw(:all);
+use Storable qw(store retrieve);
+use File::Spec;
+
+use Mail::SpamAssassin;
+use Mail::SpamAssassin::Plugin;
+use Mail::SpamAssassin::Util qw(untaint_file_path);
+
+our @ISA = qw(Mail::SpamAssassin::Plugin);
+
+sub dbg { my $msg = shift; Mail::SpamAssassin::Logger::dbg("NeuralNetwork: $msg", @_); }
+sub info { my $msg = shift; Mail::SpamAssassin::Logger::info("NeuralNetwork: $msg", @_); }
+
+sub new {
+  my ($class, $mailsa) = @_;
+
+  $class = ref($class) || $class;
+  my $self = $class->SUPER::new($mailsa);
+  bless ($self, $class);
+
+  $self->set_config($mailsa->{conf});
+  $self->register_eval_rule("check_neuralnetwork_spam", $Mail::SpamAssassin::Conf::TYPE_BODY_EVALS);
+  $self->register_eval_rule("check_neuralnetwork_ham", $Mail::SpamAssassin::Conf::TYPE_BODY_EVALS);
+
+  return $self;
+}
+
+sub set_config {
+  my ($self, $conf) = @_;
+  my @cmds;
+
+=over 4
+
+=item neuralnetwork_data_dir dirname (default: undef)
+
+Where NeuralNetwork plugin will store its data.
+
+=item neuralnetwork_min_text_len n (default: 256)
+
+Minimum number of characters of visible text required to run prediction or learning on a message.
+
+=item neuralnetwork_min_word_len n (default: 4)
+
+Minimum token length considered when building the vocabulary and feature vectors.
+
+=item neuralnetwork_max_word_len n (default: 24)
+
+Maximum token length considered when building the vocabulary and feature vectors.
+
+=item neuralnetwork_vocab_cap n (default: 10000)
+
+Maximum number of vocabulary terms to retain; least-frequent terms are pruned when exceeded.
+
+=item neuralnetwork_spam_threshold f (default: 0.8)
+
+Prediction values above this threshold are considered spam.
+
+=item neuralnetwork_ham_threshold f (default: 0.2)
+
+Prediction values below this threshold are considered ham.
+
+=item neuralnetwork_learning_rate f (default: 0.1)
+
+Learning rate used by the underlying FANN network during incremental training.
+
+=item neuralnetwork_momentum f (default: 0.1)
+
+Momentum used for training updates.
+
+=item neuralnetwork_train_epochs n (default: 50)
+
+Number of training epochs to perform when learning a single message.
+
+=item neuralnetwork_stopwords words (default: "the and for with that this from there their have be not but you your")
+
+Space-separated list of stopwords to ignore when tokenizing text.
+
+=back
+
+=cut
+
+  push(@cmds, {
+    setting => 'neuralnetwork_data_dir',
+    is_admin => 1,
+    default => undef,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_STRING,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_min_text_len',
+    is_admin => 1,
+    default => 256,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_min_word_len',
+    is_admin => 1,
+    default => 4,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_max_word_len',
+    is_admin => 1,
+    default => 24,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_vocab_cap',
+    is_admin => 1,
+    default => 10000,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_spam_threshold',
+    is_admin => 1,
+    default => 0.8,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_ham_threshold',
+    is_admin => 1,
+    default => 0.2,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_learning_rate',
+    is_admin => 1,
+    default => 0.1,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_momentum',
+    is_admin => 1,
+    default => 0.1,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_train_epochs',
+    is_admin => 1,
+    default => 50,
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_NUMERIC,
+  });
+  push(@cmds, {
+    setting => 'neuralnetwork_stopwords',
+    is_admin => 1,
+    default => 'the and for with that this from there their have be not but you your',
+    type => $Mail::SpamAssassin::Conf::CONF_TYPE_STRING,
+  });
+
+  $conf->{parser}->register_commands(\@cmds);
+}
+
+sub finish_parsing_end {
+  my ($self, $opts) = @_;
+
+  my $nn_data_dir = $self->{main}->{conf}->{neuralnetwork_data_dir};
+  return unless defined $nn_data_dir;
+
+  $nn_data_dir = Mail::SpamAssassin::Util::untaint_file_path($nn_data_dir);
+  if (not -d $nn_data_dir) {
+    dbg("neuralnetwork_data_dir is invalid");
+    return;
+  }
+
+  my $dataset_path = File::Spec->catfile($nn_data_dir, 'fann.model');
+  if (-f $dataset_path) {
+    eval {
+      $self->{neural_model} = AI::FANN->new_from_file($dataset_path);
+      1;
+    } or do {
+      my $err = $@ || 'unknown';
+      info("Failed to load neural model from $dataset_path: $err");
+    };
+  }
+}
+
+# Helper: extract first visible text part
+sub _get_first_visible_text {
+  my ($msg) = @_;
+  return unless defined $msg;
+  my $email_text = '';
+  my @parts = $msg->find_parts(qr@^text/(?:html|plain)$@);
+  foreach my $part (@parts) {
+    next if not exists $part->{visible_rendered};
+    $email_text = $part->{visible_rendered} || '';
+    last;
+  }
+  return $email_text;
+}
+
+# Converts a list of raw text strings into a list of
+# numerical feature vectors (dense arrays), suitable for Neural Networks training.
+sub _text_to_features {
+    my ($conf, $nn_data_dir, $train, $label, @emails) = @_;
+
+    my $MIN_WORD_LEN = $conf->{neuralnetwork_min_word_len};
+    my $MAX_WORD_LEN = $conf->{neuralnetwork_max_word_len};
+    my $VOCAB_CAP    = $conf->{neuralnetwork_vocab_cap};
+    my %STOPWORDS    = map { lc($_) => 1 } split /\s+/, $conf->{neuralnetwork_stopwords};
+    my $STOPWORDS_REF = \%STOPWORDS;
+
+    return unless defined $nn_data_dir;
+    $nn_data_dir = Mail::SpamAssassin::Util::untaint_file_path($nn_data_dir);
+
+    if( not -d $nn_data_dir) {
+      info("Cannot access directory $nn_data_dir");
+      return;
+    }
+
+    # Read the vocabulary (format: { terms => {word => {total=>n,docs=>m,spam=>s,ham=>h}}, _doc_count => N, _spam_count => S, _ham_count => H })
+    my %vocabulary;
+    my $vocab_path = File::Spec->catfile($nn_data_dir, 'vocabulary.data');
+    if(-f $vocab_path) {
+      eval {
+        my $ref = retrieve($vocab_path);
+        if (ref $ref eq 'HASH') {
+          %vocabulary = %{$ref};
+        }
+        1;
+      } or do {
+        warn("Failed to retrieve vocabulary from $vocab_path: " . ($@ || 'unknown'));
+      };
+    }
+
+    $vocabulary{terms} ||= {};
+    $vocabulary{_doc_count} ||= 0;
+    $vocabulary{_spam_count} ||= 0;
+    $vocabulary{_ham_count} ||= 0;
+
+    # tokenize helper
+    my $tokenize = sub {
+      my ($text) = @_;
+      return () unless defined $text;
+      $text = lc $text;
+      # replace HTML entities and punctuation with spaces
+      $text =~ s/&[a-z#0-9]+;/ /g;
+      $text =~ s{[^\p{L}\p{N}\-]}{ }g;
+      my @tokens = grep { length($_) >= $MIN_WORD_LEN && length($_) <= $MAX_WORD_LEN } split /\s+/, $text;
+      @tokens = grep { $_ !~ /^\d+$/ } @tokens;         # drop pure numbers
+      @tokens = grep { !$STOPWORDS_REF->{$_} } @tokens;     # drop stopwords
+      return @tokens;
+    };
+
+    # When training, build per-document term sets to update doc counts
+    my $local_doc_increment = 0;
+    if ($train) {
+      foreach my $email_text (@emails) {
+        next unless defined $email_text;
+        my @tokens = $tokenize->($email_text);
+        next unless @tokens;
+        $local_doc_increment++;
+
+        # count doc-level presence once per unique token
+        my %seen;
+        foreach my $t (@tokens) {
+          $vocabulary{terms}{$t}{total} = ($vocabulary{terms}{$t}{total} || 0) + 1;
+          $seen{$t} = 1;
+        }
+        foreach my $t (keys %seen) {
+          $vocabulary{terms}{$t}{docs} = ($vocabulary{terms}{$t}{docs} || 0) + 1;
+          # Track spam/ham sources
+          if (defined $label && $label == 1) {
+            $vocabulary{terms}{$t}{spam} = ($vocabulary{terms}{$t}{spam} || 0) + 1;
+          } elsif (defined $label && $label == 0) {
+            $vocabulary{terms}{$t}{ham} = ($vocabulary{terms}{$t}{ham} || 0) + 1;
+          }
+        }
+      }
+
+      # increment global doc count
+      $vocabulary{_doc_count} += $local_doc_increment if $local_doc_increment > 0;
+
+      # increment spam/ham counters
+      if (defined $label && $label == 1 && $local_doc_increment > 0) {
+        $vocabulary{_spam_count} += $local_doc_increment;
+      } elsif (defined $label && $label == 0 && $local_doc_increment > 0) {
+        $vocabulary{_ham_count} += $local_doc_increment;
+      }
+
+      # Prune vocabulary if needed: keep top VOCAB_CAP by total count
+      my $terms_count = scalar keys %{ $vocabulary{terms} };
+      if ($terms_count > $VOCAB_CAP) {
+        my @top = sort { ($vocabulary{terms}{$b}{total}||0) <=> ($vocabulary{terms}{$a}{total}||0) } keys %{ $vocabulary{terms} };
+        my %pruned;
+        for my $i (0 .. $VOCAB_CAP-1) {
+          last unless defined $top[$i];
+          $pruned{$top[$i]} = $vocabulary{terms}{$top[$i]};
+        }
+        $vocabulary{terms} = \%pruned;
+      }
+
+      # Persist vocabulary
+      $vocab_path = Mail::SpamAssassin::Util::untaint_file_path($vocab_path);
+      eval {
+        store(\%vocabulary, $vocab_path) or die "store failed";
+        1;
+      } or do {
+        warn("Failed to store vocabulary to $vocab_path: " . ($@ || 'unknown'));
+      };
+    }
+
+    # Build vocabulary index (stable sorted order)
+    my @vocab_keys = sort keys %{ $vocabulary{terms} };
+    my %vocab_index = map { $vocab_keys[$_] => $_ } 0..$#vocab_keys;
+    my $vocab_size = scalar @vocab_keys;
+    return ([], 0) unless $vocab_size > 0;
+
+    # Precompute IDF: log((N+1)/(df+1)) + 1 smoothing
+    my $N = $vocabulary{_doc_count} || 1;
+    my %idf;
+    foreach my $w (@vocab_keys) {
+      my $df = $vocabulary{terms}{$w}{docs} || 0;
+      $idf{$w} = log( ($N + 1) / ($df + 1) ) + 1;
+    }
+
+    # Create TF-IDF vectors and L2-normalize
+    my @feature_vectors;
+    foreach my $email_text (@emails) {
+      next unless defined $email_text;
+      my @tokens = $tokenize->($email_text);
+      my %tf;
+      $tf{$_}++ for @tokens;
+      # Build raw tf-idf vector
+      my @vec = (0) x $vocab_size;
+      foreach my $term (keys %tf) {
+        next unless exists $vocab_index{$term};
+        my $i = $vocab_index{$term};
+        my $tf_val = $tf{$term} / (scalar @tokens || 1);  # normalized TF
+        $vec[$i] = $tf_val * ($idf{$term} || 1);
+      }
+      # L2 normalization
+      my $norm = 0;
+      $norm += $_ * $_ for @vec;
+      $norm = sqrt($norm) || 1;
+      @vec = map { $_ / $norm } @vec;
+
+      push @feature_vectors, \@vec;
+    }
+
+    return \@feature_vectors, $vocab_size;
+}
+
+sub learn_message {
+  my ($self, $params) = @_;
+  my $isspam = $params->{isspam};
+  my $msg = $params->{msg};
+
+  my $nn_data_dir = $self->{main}->{conf}->{neuralnetwork_data_dir};
+  unless (defined $nn_data_dir) {
+    dbg("neuralnetwork_data_dir not set");
+    return;
+  }
+  $nn_data_dir = Mail::SpamAssassin::Util::untaint_file_path($nn_data_dir);
+  if (not -d $nn_data_dir) {
+    info("Invalid neuralnetwork_data_dir path");
+    return;
+  }
+
+  if( not defined $isspam ) {
+    dbg("Unknown spam value");
+    return;
+  }
+
+  my $conf = $self->{main}->{conf};
+  my $MIN_TEXT_LEN = $conf->{neuralnetwork_min_text_len};
+  my $LEARNING_RATE = $conf->{neuralnetwork_learning_rate};
+  my $MOMENTUM = $conf->{neuralnetwork_momentum};
+  my $TRAIN_EPOCHS = $conf->{neuralnetwork_train_epochs};
+
+  my @training_data;
+  if(defined $msg) {
+    my $text = _get_first_visible_text($msg);
+    if (!defined $text || length($text) < $MIN_TEXT_LEN) {
+      dbg("Not enough text, skipping neural network processing");
+      return;
+    }
+    push(@training_data, { label => $isspam, text => $text } );
+  }
+
+  my $dataset_path = File::Spec->catfile($nn_data_dir, 'fann.model');
+
+  # Extract the text and labels
+  my @email_texts = map { $_->{text} } @training_data;
+  my @labels = map { $_->{label} } @training_data;
+
+  # Update the vocabulary
+  my $update_vocab = 1;
+
+  # Convert email text to numerical feature vectors
+  my ($feature_vectors, $vocab_size) = _text_to_features($self->{main}->{conf}, $nn_data_dir, $update_vocab, $isspam, @email_texts);
+
+  return unless $feature_vectors && @$feature_vectors;
+
+  my $num_input = scalar(@{$feature_vectors->[0]});
+  if ($num_input == 0) {
+    dbg("No valid features found in message, skipping learning");
+    return;
+  }
+  my $num_hidden_neurons = int(sqrt($num_input)) || 1;
+  my $num_output_neurons = 1;
+
+  my $network;
+  if(defined $self->{neural_model} && $self->{neural_model}->num_inputs() == $num_input) {
+    $network = $self->{neural_model};
+  } else {
+    $network = AI::FANN->new_standard($num_input, $num_hidden_neurons, $num_output_neurons);
+    $network->hidden_activation_function(FANN_SIGMOID);
+    $network->output_activation_function(FANN_SIGMOID);
+  }
+  $network->learning_rate($LEARNING_RATE);
+  $network->learning_momentum($MOMENTUM);
+
+  # Use multiple-epoch incremental training for each feature vector to increase learning effect
+  my $epochs = $TRAIN_EPOCHS;
+  for my $e (1 .. $epochs) {
+    for my $i (0 .. $#$feature_vectors) {
+      my $input = $feature_vectors->[$i];
+      my $output = [$labels[$i] ? 1 : 0];
+      eval { $network->train($input, $output); 1 } or dbg("Training step failed: " . ($@ || 'unknown'));
+    }
+  }
+
+  if (scalar(@$feature_vectors) == 1) {
+    my $pred_after = eval { $network->run($feature_vectors->[0]) };
+    $pred_after = ref($pred_after) ? $pred_after->[0] : $pred_after;
+    dbg("Prediction after learning: " . (defined $pred_after ? $pred_after : 'undef'));
+  }
+
+  # Save the model
+  eval {
+    $network->save($dataset_path) or die "save failed";
+    1;
+  } and do {
+    dbg("Model saved to '$dataset_path' (input:$num_input)");
+    $self->{neural_model} = $network;
+  } or do {
+    info("Cannot save model to '$dataset_path' (" . ($@ || 'unknown') . ")");
+  };
+  return;
+}
+
+sub check_neuralnetwork_spam {
+  my ($self, $pms) = @_;
+  
+  _check_neuralnetwork($self, $pms);
+  return $pms->{neuralnetwork_spam};
+}
+
+sub check_neuralnetwork_ham {
+  my ($self, $pms) = @_;
+  
+  _check_neuralnetwork($self, $pms);
+  return $pms->{neuralnetwork_ham};
+}
+
+# Helper: ensure vector matches expected size by padding/truncating
+sub _adjust_vector_size {
+  my ($vec, $expected) = @_;
+  return unless defined $vec && defined $expected && $expected >= 0;
+  my @v = @$vec;
+  my $len = scalar @v;
+  if ($len < $expected) {
+    dbg("Adjusting input vector: padding from $len to $expected");
+    push @v, (0) x ($expected - $len);
+  } elsif ($len > $expected) {
+    dbg("Adjusting input vector: truncating from $len to $expected");
+    $#v = $expected - 1;
+  }
+  return \@v;
+}
+
+sub _check_neuralnetwork {
+  my ($self, $pms) = @_;
+
+  return 0 if (!$self->{main}->{conf}->{use_learner});
+  my $msg = $pms->{msg};
+
+  if(exists $pms->{neuralnetwork_prediction}) {
+    return;
+  }
+
+  my $conf = $self->{main}->{conf};
+  my $MIN_TEXT_LEN = $conf->{neuralnetwork_min_text_len};
+  my $SPAM_THRESHOLD = $conf->{neuralnetwork_spam_threshold};
+  my $HAM_THRESHOLD  = $conf->{neuralnetwork_ham_threshold};
+
+  my $email_to_predict = _get_first_visible_text($msg);
+  if(!defined $email_to_predict || length($email_to_predict) < $MIN_TEXT_LEN) {
+    $pms->{neuralnetwork_prediction} = undef;
+    dbg("Too short email text");
+    return;
+  }
+
+  my $nn_data_dir = $self->{main}->{conf}->{neuralnetwork_data_dir};
+  $nn_data_dir = Mail::SpamAssassin::Util::untaint_file_path($nn_data_dir);
+  if (not -d $nn_data_dir) {
+    $pms->{neuralnetwork_prediction} = undef;
+    info("Invalid neuralnetwork_data_dir path");
+    return;
+  }
+
+  # Do not update the vocabulary
+  my $update_vocab = 0;
+
+  # Convert email to feature vector using the same vocabulary
+  my ($feature_vectors, $vocab_size) = _text_to_features($conf, $nn_data_dir, $update_vocab, undef, $email_to_predict);
+  unless ($feature_vectors && @$feature_vectors) {
+    $pms->{neuralnetwork_prediction} = undef;
+    dbg("Not enough tokens found");
+    return;
+  }
+  my $input_vector = $feature_vectors->[0];
+
+  my $dataset_path = File::Spec->catfile($nn_data_dir, 'fann.model');
+  if(not -f $dataset_path) {
+    $pms->{neuralnetwork_prediction} = undef;
+    dbg("Can't predict without a trained model, $dataset_path cannot be read");
+    return;
+  }
+
+  if (!defined $self->{neural_model}) {
+    eval {
+      $self->{neural_model} = AI::FANN->new_from_file($dataset_path);
+      1;
+    } or do {
+      dbg("Failed to load model for prediction: " . ($@ || 'unknown'));
+      return;
+    };
+  }
+  my $network = $self->{neural_model};
+
+  my $expected_size = $network->num_inputs();
+  if (scalar(@$input_vector) != $expected_size) {
+    dbg("Prediction vector size mismatch. Got ".scalar(@$input_vector).", expected ".$expected_size.". Adjusting vector.");
+    $input_vector = _adjust_vector_size($input_vector, $expected_size);
+    # If adjustment failed for some reason, abort
+    unless (defined $input_vector && scalar(@$input_vector) == $expected_size) {
+      $pms->{neuralnetwork_prediction} = undef;
+      info("Adjusted vector invalid, skipping prediction.");
+      return;
+    }
+  }
+
+  my $prediction = eval { $network->run($input_vector) } ;
+  if ($@) {
+    $pms->{neuralnetwork_prediction} = undef;
+    dbg("Prediction failed: $@");
+    return;
+  }
+  $prediction = ref($prediction) ? $prediction->[0] : $prediction;
+
+  unless(defined $prediction) {
+    dbg("No prediction available");
+    $pms->{neuralnetwork_prediction} = undef;
+    return;
+  }
+
+  if ($prediction > $SPAM_THRESHOLD) {
+    $pms->{neuralnetwork_spam} = 1;
+    dbg("Prediction for email : spam ($prediction)");
+  } elsif ($prediction < $HAM_THRESHOLD) {
+    $pms->{neuralnetwork_ham} = 1;
+    dbg("Prediction for email : ham ($prediction)");
+  } else {
+    dbg("Prediction for email : unknown ($prediction)");
+  }
+  $pms->{neuralnetwork_prediction} = $prediction;
+  return;
+}
+
+1;
